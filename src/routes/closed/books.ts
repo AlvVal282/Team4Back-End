f4b43714ab2945c9a4e4a69596ed840b7dd6a733
westerntoad <abr.engebretson@gmail.com>
2024-11-01 20:04:56 -0700

    /title/:name documentation complete

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -5,22 +5,88 @@ import { IJwtRequest } from '../../core/models';
 // retrieve the router object from express
 const booksRouter : Router = express.Router();
 
+/*
+interface: IRatings {
+    average: number;
+    count: number;
+    rating_1: number;
+    rating_2: number;
+    rating_3: number;
+    rating_4: number;
+    rating_5: number;
+}
+
+interface: IUrlIcon {
+    large: string;
+    small: string;
+}
+
+interface: IBook {
+    isbn13: number;
+    authors: string;
+    publication: number;
+    original_title: string;
+    title: string;
+    ratings: IRatings;
+    icons: IUrlIcon;
+}
+*/
+
 /**
- * @api {get} /books/title Request book by title
+ * @api {get} /books/title/:name Request book by title
  * @apiName GetBookByTitle
  * @apiGroup Books
- * @apiDescription Retrieve all books that have a specified keyword in its Title.
+ * @apiDescription Retrieve all books that contain a specified string in its title. If
+ * multiple books are returned by this query, it returns them in alphabetical order.
  *
- * @apiSuccess {Object[]} results An aggregate of all books that match the query
- * @apiSuccess {string} results.name The name of the book
+ * @apiParam {string} name A keyword to query all book titles.
+ *
+ * @apiSuccess {Object[]} results An aggregate of all books that match the query.
+ * @apiSuccess {number} results.isbn13 The ISBN number for the book.
+ * @apiSuccess {string} results.author A comma-separated string of authors who have
+ * contributed to the book.
+ * @apiSuccess {number} results.publication The initial publication date of this book.
+ * @apiSuccess {string} results.original_title The title of the series this book was
+ * printed in. If not in a series, it is a copy of the title attribute. Note that you
+ * cannot consisently rely upon <code>original_title</code> to be applied applicably to
+ * serial publications.
+ * @apiSuccess {string} results.title The title of the book.
+ * @apiSuccess {Object} results.ratings An object representing all the information for
+ * consumer and critic ratings for the given book.
+ * @apiSuccess {number} results.ratings.average The mean value of all 5-star ratings for
+ * this book.
+ * @apiSuccess {number} results.ratings.count The total number of ratings for this book.
+ * @apiSuccess {number} results.ratings.rating1 The total number of 1-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating2 The total number of 2-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating3 The total number of 3-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating4 The total number of 4-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating5 The total number of 5-star ratings for this book.
+ * @apiSuccess {Object} results.icons An object holding the urls for the images of this book.
+ * 98x147
+ * 50x75
+ * @apiSuccess {string} results.icons.large The url whose destination matches an
+ * image for this book. On average, image sizes fall within about <code>98x147</code>
+ * in pixels.
+ * @apiSuccess {string} results.icons.small The url whose destination matches the
+ * image for this book. On average, image sizes fall within about <code>50x75</code>
  *
  * @apiError (404: No book with given title) {String} message "No book with given title"
+ * @apiError (400: Empty query parameter) {String} message "No query parameter in url"
  */
-booksRouter.get('/title', (request: IJwtRequest, response: Response) => {
+booksRouter.get('/title/:name', (request: IJwtRequest, response: Response) => {
     response.status(500).send({
         message:
             'Route not currently implemented. Please complain to developers.',
     });
 });
+
+/*
+booksRouter.get('/title/:name', (request: IJwtRequest, response: Response) => {
+    response.status(500).send({
+        message:
+            'Route not currently implemented. Please complain to developers.',
+    });
+});
+*/
 
 export { booksRouter };
