b27d07571c60c71cbce710848b68673d4307ce07
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2024-11-04 13:20:58 -0800

    Added doc for rating

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -82,6 +82,56 @@ booksRouter.get('/:isbn', (request: IJwtRequest, response: Response) => {
     });
 });
 
+/**
+ * @api {get} /books/:rating Request book by rating
+ * @apiName GetBookByRating
+ * @apiGroup Books
+ * @apiDescription Retrieve a book from the database that reach a minimum rating
+ * threshold.
+ *
+ * @apiParam {float} rating A rating float. Must be within the range of
+ * <code> 1 <= 5 </code>
+ *
+ * @apiSuccess {Object[]} results An aggregate of all books that reach the minimum
+ * rating.
+ * @apiSuccess {number} result.isbn13 The ISBN number for the book.
+ * @apiSuccess {string} result.author A comma-separated string of authors who have
+ * contributed to the book.
+ * @apiSuccess {number} result.publication The initial publication date of this book.
+ * @apiSuccess {string} result.original_title The title of the series this book was
+ * printed in. If not in a series, it is a copy of the title attribute. Note that you
+ * cannot consisently rely upon <code>original_title</code> to be applied applicably to
+ * serial publications.
+ * @apiSuccess {string} result.title The title of the book.
+ * @apiSuccess {Object} result.ratings An object representing all the information for
+ * consumer and critic ratings for the given book.
+ * @apiSuccess {number} result.ratings.average The mean value of all 5-star ratings for
+ * this book.
+ * @apiSuccess {number} result.ratings.count The total number of ratings for this book.
+ * @apiSuccess {number} result.ratings.rating1 The total number of 1-star ratings for this book.
+ * @apiSuccess {number} result.ratings.rating2 The total number of 2-star ratings for this book.
+ * @apiSuccess {number} result.ratings.rating3 The total number of 3-star ratings for this book.
+ * @apiSuccess {number} result.ratings.rating4 The total number of 4-star ratings for this book.
+ * @apiSuccess {number} result.ratings.rating5 The total number of 5-star ratings for this book.
+ * @apiSuccess {Object} result.icons An object holding the urls for the images of this book.
+ * @apiSuccess {string} result.icons.large The url whose destination matches an
+ * image for this book. On average, image sizes fall within about <code>98x147</code>
+ * in pixels.
+ * @apiSuccess {string} result.icons.small The url whose destination matches the
+ * image for this book. On average, image sizes fall within about <code>50x75</code>
+ *
+ * @apiError (404: No book with given ISBN) {String} message "No book with given ISBN"
+ * @apiError (400: Query parameter wrong type) {String} message "Query parameter not of required type - please refer to documentation"
+ * @apiError (400: Rating not in range) {String} message "Rating not in range - please refer to documentation"
+ * @apiError (400: Empty query parameter) {String} message "No query parameter in url"
+ */
+booksRouter.get('/:rating', (request: IJwtRequest, response: Response) => {
+    response.status(500).send({
+        message:
+            'Route not currently implemented. Please complain to developers.',
+    });
+});
+
 
 /**
  * @api {get} /books/title/:name Request books by title
