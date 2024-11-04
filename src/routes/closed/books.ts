22c7571f9d41c61476e4b8ee92667a2f3ed7d94b
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2024-11-04 13:40:29 -0800

    Added request books by author documentation

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -87,7 +87,8 @@ booksRouter.get('/:isbn', (request: IJwtRequest, response: Response) => {
  * @apiName GetBookByRating
  * @apiGroup Books
  * @apiDescription Retrieve a book from the database that reach a minimum rating
- * threshold.
+ * threshold. If multiple books are returned by this query, it returns them in 
+ * alphabetical order.
  *
  * @apiParam {float} rating A rating float. Must be within the range of
  * <code> 1 <= 5 </code>
@@ -179,5 +180,51 @@ booksRouter.get('/title/:name', (request: IJwtRequest, response: Response) => {
     });
 });
 
+
+/**
+ * @api {get} /books/:author Request books by author
+ * @apiName GetBooksByAuthor
+ * @apiGroup Books
+ * @apiDescription Retrieve all books that were written by a specified author. If
+ * multiple books are returned by this query, it returns them in alphabetical order.
+ *
+ * @apiParam {string} author A author to query all books.
+ *
+ * @apiSuccess {Object[]} results An aggregate of all books that match the query.
+ * @apiSuccess {number} results.isbn13 The ISBN number for the book.
+ * @apiSuccess {string} results.author A comma-separated string of authors who have
+ * contributed to the book.
+ * @apiSuccess {number} results.publication The initial publication date of this book.
+ * @apiSuccess {string} results.original_title The title of the series this book was
+ * printed in. If not in a series, it is a copy of the title attribute. Note that you
+ * cannot consisently rely upon <code>original_title</code> to be applied applicably to
+ * serial publications.
+ * @apiSuccess {string} results.title The title of the book.
+ * @apiSuccess {Object} results.ratings An object representing all the information for
+ * consumer and critic ratings for the given book.
+ * @apiSuccess {number} results.ratings.average The mean value of all 5-star ratings for
+ * this book.
+ * @apiSuccess {number} results.ratings.count The total number of ratings for this book.
+ * @apiSuccess {number} results.ratings.rating1 The total number of 1-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating2 The total number of 2-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating3 The total number of 3-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating4 The total number of 4-star ratings for this book.
+ * @apiSuccess {number} results.ratings.rating5 The total number of 5-star ratings for this book.
+ * @apiSuccess {Object} results.icons An object holding the urls for the images of this book.
+ * @apiSuccess {string} results.icons.large The url whose destination matches an
+ * image for this book. On average, image sizes fall within about <code>98x147</code>
+ * in pixels.
+ * @apiSuccess {string} results.icons.small The url whose destination matches the
+ * image for this book. On average, image sizes fall within about <code>50x75</code>
+ *
+ * @apiError (404: No book with given title) {String} message "No book with given author"
+ * @apiError (400: Empty query parameter) {String} message "No query parameter in url"
+ */
+booksRouter.get('/:author', (request: IJwtRequest, response: Response) => {
+    response.status(500).send({
+        message:
+            'Route not currently implemented. Please complain to developers.',
+    });
+});
 
 export { booksRouter };
