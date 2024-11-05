2a31266c9cd27294401988b296d3ad1525a347c7
cnlwebber <cnlwebber@gmail.com>
2024-11-04 19:14:27 -0800

    sql.init working

--- data/init.sql
+++ data/init.sql
@@ -1,72 +1,74 @@
--- Active: 1710457548247@@127.0.0.1@5432@tcss460@public
-
-CREATE TABLE Demo (DemoID SERIAL PRIMARY KEY,
-                        Priority INT,
-                        Name TEXT NOT NULL UNIQUE,
-                        Message TEXT
+CREATE TABLE Demo (
+    DemoID SERIAL PRIMARY KEY,
+    Priority INT,
+    Name TEXT NOT NULL UNIQUE,
+    Message TEXT
 );
 
-CREATE TABLE Account (Account_ID SERIAL PRIMARY KEY,
-                      FirstName VARCHAR(255) NOT NULL,
-		              LastName VARCHAR(255) NOT NULL,
-                      Username VARCHAR(255) NOT NULL UNIQUE,
-                      Email VARCHAR(255) NOT NULL UNIQUE,
-                      Phone VARCHAR(15) NOT NULL UNIQUE,
-                      Account_Role int NOT NULL
+CREATE TABLE Account (
+    Account_ID SERIAL PRIMARY KEY,
+    FirstName VARCHAR(255) NOT NULL,
+    LastName VARCHAR(255) NOT NULL,
+    Username VARCHAR(255) NOT NULL UNIQUE,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Phone VARCHAR(15) NOT NULL UNIQUE,
+    Account_Role INT NOT NULL
 );
 
-
-CREATE TABLE Account_Credential (Credential_ID SERIAL PRIMARY KEY,
-                      Account_ID INT NOT NULL,
-                      Salted_Hash VARCHAR(255) NOT NULL,
-                      salt VARCHAR(255),
-                      FOREIGN KEY(Account_ID) REFERENCES Account(Account_ID)
+CREATE TABLE Account_Credential (
+    Credential_ID SERIAL PRIMARY KEY,
+    Account_ID INT NOT NULL,
+    Salted_Hash VARCHAR(255) NOT NULL,
+    salt VARCHAR(255),
+    FOREIGN KEY(Account_ID) REFERENCES Account(Account_ID)
 );
 
-CREATE TABLE temp (id INT PRIMARY KEY,
-        isbn13 BIGINT,
-        authors TEXT,
-        publication_year INT,
-        original_title TEXT,
-        title TEXT,
-        rating_avg FLOAT,
-        rating_count INT,
-        rating_1_star INT,
-        rating_2_star INT,
-        rating_3_star INT,
-        rating_4_star INT,
-        rating_5_star INT,
-        image_url TEXT,
-        image_small_url TEXT
+CREATE TABLE temp (
+    id SERIAL PRIMARY KEY,
+    isbn13 BIGINT,
+    authors TEXT,
+    publication_year INT,
+    original_title TEXT,
+    title TEXT,
+    rating_avg FLOAT,
+    rating_count INT,
+    rating_1_star INT,
+    rating_2_star INT,
+    rating_3_star INT,
+    rating_4_star INT,
+    rating_5_star INT,
+    image_url TEXT,
+    image_small_url TEXT
 );
 
-CREATE TABLE Books (id INT PRIMARY KEY,
-        isbn13 BIGINT,
-        publication_year INT,
-        original_title TEXT,
-        title TEXT,
-        rating_avg FLOAT,
-        rating_count INT,
-        rating_1_star INT,
-        rating_2_star INT,
-        rating_3_star INT,
-        rating_4_star INT,
-        rating_5_star INT,
-        image_url TEXT,
-        image_small_url TEXT
+CREATE TABLE Books (
+    id SERIAL PRIMARY KEY,
+    isbn13 BIGINT UNIQUE,
+    publication_year INT,
+    original_title TEXT,
+    title TEXT,
+    rating_avg FLOAT,
+    rating_count INT,
+    rating_1_star INT,
+    rating_2_star INT,
+    rating_3_star INT,
+    rating_4_star INT,
+    rating_5_star INT,
+    image_url TEXT,
+    image_small_url TEXT
 );
-	
+
 CREATE TABLE Author (
-	Author_ID SERIAL PRIMARY KEY,
-	Author_Name TEXT NOT NULL UNIQUE
+    Author_ID SERIAL PRIMARY KEY,
+    Author_Name TEXT NOT NULL UNIQUE
 );
-	
+
 CREATE TABLE Books_Authors (
-	isbn13 BIGINT NOT NULL,
-	Author_ID INT NOT NULL,
-	FOREIGN KEY (isbn13) REFERENCES Books(isbn13),
-	FOREIGN KEY (Author_ID) REFERENCES Authors(Author_ID),
-	PRIMARY KEY (Author_ID, isbn13)
+    isbn13 BIGINT NOT NULL,
+    Author_ID INT NOT NULL,
+    FOREIGN KEY (isbn13) REFERENCES Books(isbn13),
+    FOREIGN KEY (Author_ID) REFERENCES Author(Author_ID),
+    PRIMARY KEY (Author_ID, isbn13)
 );
 
 COPY temp
@@ -76,15 +78,15 @@ CSV HEADER;
 
 INSERT INTO Books (isbn13, publication_year, original_title, title, rating_avg, rating_count, rating_1_star, rating_2_star, rating_3_star, rating_4_star, rating_5_star, image_url, image_small_url)
 SELECT isbn13, publication_year, original_title, title, rating_avg, rating_count, rating_1_star, rating_2_star, rating_3_star, rating_4_star, rating_5_star, image_url, image_small_url
-FROM temp
+FROM temp;
 
-INSERT Author (Author_Name)
+INSERT INTO Author (Author_Name)
 SELECT DISTINCT unnest(string_to_array(authors, ','))
-FROM temp
+FROM temp;
 
 INSERT INTO Books_Authors (isbn13, Author_ID)
 SELECT t.isbn13, a.Author_ID
 FROM temp t
-JOIN Author a ON a.Author_Name = ANY(string_to_array(t.authors, ',')) 
+JOIN Author a ON a.Author_Name = ANY(string_to_array(t.authors, ','));
 
-drop table temp
+DROP TABLE IF EXISTS temp;
